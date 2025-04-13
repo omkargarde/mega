1400abed03c2651112d1d2327f83f8b6312c6fa4
omkargarde <omyagarde@gmail.com>
2025-04-13 15:09:04 +0000

    feat: Refactor auth flow and remove try-catch blocks in favor of asyncHandler

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from "express";
 
-
 import type { IHttpError } from "../types/http-error.type.ts";
 
 import { HTTP_STATUS_CODES } from "../constants/status.constant.ts";
@@ -19,7 +18,6 @@ import {
   VerifyEmailToken,
   VerifyUser,
 } from "../services/user/user.service.ts";
-
 import { ApiError } from "../utils/api-error.util.ts";
 import { ApiResponse } from "../utils/api-response.util.ts";
 import { asyncHandler } from "../utils/async-handler.util.ts";
@@ -31,62 +29,48 @@ const registerUser = asyncHandler(async (req: Request, res: Response) => {
     password: string;
     username: null | string;
   };
-
-  //validation
-  try {
-    const userDoesExist = await FindUser(email);
-
-    if (userDoesExist) {
-      return res
-        .status(HTTP_STATUS_CODES.Conflict)
-        .json(
-          new ApiResponse(
-            HTTP_STATUS_CODES.Conflict,
-            "",
-            USER_MESSAGES.UserExists,
-          ),
-        );
-    }
-
-    const token = UnHashedToken();
-    const tokenExpiry = TokenExpiry();
-    const user = await CreateUser(
-      email,
-      tokenExpiry,
-      token,
-      password,
-      username,
-    );
-
-    if (!user) {
-      throw new ApiError(
-        HTTP_STATUS_CODES.InternalServerError,
-        "Failed to create user",
-      );
-    }
-    if (!user.emailVerificationToken) {
-      throw new ApiError(
-        HTTP_STATUS_CODES.InternalServerError,
-        USER_MESSAGES.FailedUserCreation,
-      );
-    }
-
-    await sendVerificationMail(user.email, user.emailVerificationToken);
-
+  const userDoesExist = await FindUser(email);
+  if (userDoesExist) {
     return res
-      .status(HTTP_STATUS_CODES.Ok)
+      .status(HTTP_STATUS_CODES.Conflict)
       .json(
         new ApiResponse(
-          HTTP_STATUS_CODES.Ok,
+          HTTP_STATUS_CODES.Conflict,
           "",
-          USER_MESSAGES.SucceededUserCreation,
+          USER_MESSAGES.UserExists,
         ),
       );
-  } catch (error) {
-    const err = error as IHttpError;
-    const status = err.statusCode ?? 500;
-    return res.status(status).json(new ApiResponse(status, "", err.message));
   }
+
+  const token = UnHashedToken();
+  const tokenExpiry = TokenExpiry();
+  const user = await CreateUser(email, tokenExpiry, token, password, username);
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (!user) {
+    throw new ApiError(
+      HTTP_STATUS_CODES.InternalServerError,
+      "Failed to create user",
+    );
+  }
+  if (!user.emailVerificationToken) {
+    throw new ApiError(
+      HTTP_STATUS_CODES.InternalServerError,
+      USER_MESSAGES.FailedUserCreation,
+    );
+  }
+
+  await sendVerificationMail(user.email, user.emailVerificationToken);
+
+  return res
+    .status(HTTP_STATUS_CODES.Ok)
+    .json(
+      new ApiResponse(
+        HTTP_STATUS_CODES.Ok,
+        "",
+        USER_MESSAGES.SucceededUserCreation,
+      ),
+    );
 });
 
 const loginUser = asyncHandler(async (req: Request, res: Response) => {
@@ -94,63 +78,50 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
     email: string;
     password: string;
   };
-  try {
-    const user = await FindUser(email);
-    if (!user) {
-
-      return res
-        .status(HTTP_STATUS_CODES.Unauthorized)
-        .json(
-          new ApiResponse(
-            HTTP_STATUS_CODES.Unauthorized,
-            "qwe",
-            USER_MESSAGES.CredFailed,
-          ),
-        );
-    }
-    if (!user.isEmailVerified) {
-      return res
-        .status(HTTP_STATUS_CODES.Unauthorized)
-        .json(
-          new ApiResponse(
-            HTTP_STATUS_CODES.Unauthorized,
-            "asd",
-            USER_MESSAGES.EmailNotVerified,
-          ),
-        );
-    }
-    const isPasswordCorrect = await ComparePassword(password, user.password);
-    if (!isPasswordCorrect) {
-      return res
-        .status(HTTP_STATUS_CODES.Unauthorized)
-        .json(
-          new ApiResponse(
-            HTTP_STATUS_CODES.Unauthorized,
-            "zxc",
-            USER_MESSAGES.EmailNotVerified,
-          ),
-        );
-    }
-
-    const accessToken = GenerateAccessToken(
-      user._id,
-      user.email,
-      user.username,
-    );
-    res.cookie(TOKEN_ENUM.AccessToken, accessToken, {
-      expires: AccessTokenExpiry(),
-      httpOnly: true,
-    });
+  const user = await FindUser(email);
+  if (!user) {
     return res
-      .status(HTTP_STATUS_CODES.Ok)
+      .status(HTTP_STATUS_CODES.Unauthorized)
       .json(
-        new ApiResponse(HTTP_STATUS_CODES.Ok, "", USER_MESSAGES.credSuccess),
+        new ApiResponse(
+          HTTP_STATUS_CODES.Unauthorized,
+          "qwe",
+          USER_MESSAGES.CredFailed,
+        ),
       );
-  } catch (error) {
-    const err = error as IHttpError;
-    const status = err.statusCode ?? 500;
-    return res.status(status).json(new ApiResponse(status, "", err.message));
   }
+  if (!user.isEmailVerified) {
+    return res
+      .status(HTTP_STATUS_CODES.Unauthorized)
+      .json(
+        new ApiResponse(
+          HTTP_STATUS_CODES.Unauthorized,
+          "asd",
+          USER_MESSAGES.EmailNotVerified,
+        ),
+      );
+  }
+  const isPasswordCorrect = await ComparePassword(password, user.password);
+  if (!isPasswordCorrect) {
+    return res
+      .status(HTTP_STATUS_CODES.Unauthorized)
+      .json(
+        new ApiResponse(
+          HTTP_STATUS_CODES.Unauthorized,
+          "zxc",
+          USER_MESSAGES.EmailNotVerified,
+        ),
+      );
+  }
+
+  const accessToken = GenerateAccessToken(user._id, user.email, user.username);
+  res.cookie(TOKEN_ENUM.AccessToken, accessToken, {
+    expires: AccessTokenExpiry(),
+    httpOnly: true,
+  });
+  return res
+    .status(HTTP_STATUS_CODES.Ok)
+    .json(new ApiResponse(HTTP_STATUS_CODES.Ok, "", USER_MESSAGES.credSuccess));
 });
 
 const logoutUser = asyncHandler(async (req: Request, res: Response) => {
@@ -161,35 +132,28 @@ const logoutUser = asyncHandler(async (req: Request, res: Response) => {
 
 const verifyEmail = asyncHandler(async (req: Request, res: Response) => {
   const { token } = req.params;
-
-  try {
-    const userVerifiedEmail = await VerifyEmailToken(token ?? "");
-    if (!userVerifiedEmail) {
-      return res
-        .status(HTTP_STATUS_CODES.BadRequest)
-        .json(
-          new ApiResponse(
-            HTTP_STATUS_CODES.BadRequest,
-            "",
-            USER_MESSAGES.BadEmailToken,
-          ),
-        );
-    }
-    if (await VerifyUser(userVerifiedEmail)) {
-      return res
-        .status(HTTP_STATUS_CODES.Ok)
-        .json(
-          new ApiResponse(
-            HTTP_STATUS_CODES.Ok,
-            "",
-            USER_MESSAGES.VerifiedUserEmail,
-          ),
-        );
-    }
-  } catch (error) {
-    const err = error as IHttpError;
-    const status = err.statusCode ?? 500;
-    return res.status(status).json(new ApiResponse(status, "", err.message));
+  const userVerifiedEmail = await VerifyEmailToken(token ?? "");
+  if (!userVerifiedEmail) {
+    return res
+      .status(HTTP_STATUS_CODES.BadRequest)
+      .json(
+        new ApiResponse(
+          HTTP_STATUS_CODES.BadRequest,
+          "",
+          USER_MESSAGES.BadEmailToken,
+        ),
+      );
+  }
+  if (await VerifyUser(userVerifiedEmail)) {
+    return res
+      .status(HTTP_STATUS_CODES.Ok)
+      .json(
+        new ApiResponse(
+          HTTP_STATUS_CODES.Ok,
+          "",
+          USER_MESSAGES.VerifiedUserEmail,
+        ),
+      );
   }
 });
 
